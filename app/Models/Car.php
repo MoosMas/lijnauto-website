54e8963c9ad3a94c6611df2b98dc55f33c456900
Sam Brands <d286050@edu.rocwb.nl>
2025-01-10 14:25:59 +0100

    Added scopes to Car and Log models
    Logs can now be filtered by level, last 24 hours and the last week.

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,5 +21,36 @@ class Car extends Model
     public function logs()
     {
         return $this->hasMany(Log::class)->orderBy('timestamp', 'desc');
+    }
+
+    /**
+     * Get the car's logs with a specific level
+     *
+     * @param  string  $level The log level
+     * @return Builder
+     */
+    public function logLevel(string $level)
+    {
+        return $this->logs()->level($level);
+    }
+
+    /**
+     * Get the car's logs from the last 24 hours
+     *
+     * @return Builder
+     */
+    public function logsLast24Hours()
+    {
+        return $this->logs()->last24Hours();
+    }
+
+    /**
+     * Get the car's logs from the last week
+     *
+     * @return Builder
+     */
+    public function logsLastWeek()
+    {
+        return $this->logs()->lastWeek();
     }
 }
--- app/Models/Log.php
+++ app/Models/Log.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -37,5 +39,39 @@ class Log extends Model
     public function car()
     {
         return $this->belongsTo(Car::class);
+    }
+
+    /**
+     * Get the logs with a specific level
+     *
+     * @param  Builder  $query
+     * @param  string  $level The log level
+     * @return Builder
+     */
+    public function scopeLevel(Builder $query, string $level)
+    {
+        return $query->where('level', $level);
+    }
+
+    /**
+     * Get the logs from the last 24 hours
+     *
+     * @param  Builder  $query
+     * @return Builder
+     */
+    public function scopeLast24Hours(Builder $query)
+    {
+        return $query->where('timestamp', '>=', Carbon::now()->subHours(24));
+    }
+
+    /**
+     * Get the logs from the last week
+     *
+     * @param  Builder  $query
+     * @return Builder
+     */
+    public function scopeLastWeek(Builder $query)
+    {
+        return $query->where('timestamp', '>=', Carbon::now()->subWeek());
     }
 }
